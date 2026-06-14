4f26f5e54b370c9b9c183cb2959e24294a772d90
Noel De Martin <noel@noeldemartin.com>
2026-06-14 08:54:11 +0200

    Add footer with version

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="p-2 text-center">
+        <Link size="small" class="text-gray-700" :href="$app.sourceUrl">
+            {{ $app.versionName }}
+        </Link>
+    </footer>
+</template>
